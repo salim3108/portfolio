97cf7dfed007aa1e3ebd5dbeb758ea7942b8ee6f
salym <75375955+salim3108@users.noreply.github.com>
2024-02-26 21:54:16 +0300

    user input

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name: ")
+age = input("Enter your age: ")
+loc = input("Enter your location: ")
+
+print(f"Hello, your name is {name}, your {age} years old and you live in {loc}")
